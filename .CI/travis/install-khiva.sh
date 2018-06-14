46b37dedd8f45b82b555a0f5c7b421e762c71e69
David Cuesta <david.cuesta@shapelets.io>
2018-06-14 17:03:04 +0200

    khiva-install modified

--- .CI/travis/install-khiva.sh
+++ .CI/travis/install-khiva.sh
@@ -3,4 +3,4 @@
 # Installing khiva
 
 wget https://github.com/shapelets/khiva/releases/download/v0.1.0/khiva-khiva_0.1.0_amd64.deb
-dpkg -i khiva-khiva_0.1.0_amd64.deb
+sudo dpkg -i khiva-khiva_0.1.0_amd64.deb
