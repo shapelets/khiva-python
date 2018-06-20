cc98d1447f26eec5bddd7bc43c4fab92c871c530
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-20 17:18:28 +0200

    clang and moving python installation before

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -6,17 +6,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew upgrade pyenv
-    export PATH=$HOME/.pyenv/shims:$PATH
-    export PYTHON_VERSION=$(echo $TRAVIS_PYTHON_VERSION | awk -F'.' '{print $1 "." $2}')
-    pyenv install ${TRAVIS_PYTHON_VERSION}
-    pyenv init -
-
-    pyenv local ${TRAVIS_PYTHON_VERSION}
-
-    which python${PYTHON_VERSION}
-    python${PYTHON_VERSION} --version
-
     sudo pip${PYTHON_VERSION} install -r requirements.txt
     sudo pip${PYTHON_VERSION} install -r test-requirements.txt
     sudo pip${PYTHON_VERSION} install codecov
