235b543458247d36ccb1214c21277518883050d0
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-20 12:47:51 +0200

    removing docker usage in Travis (#13)
    Removing docker usage in Travis

---
+++ .CI/travis/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright (c) 2018 Shapelets.io
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+pip install -r requirements.txt
+pip install -r test-requirements.txt
+pip install codecov
