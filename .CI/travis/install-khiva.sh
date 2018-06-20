859b31828894ce0a4406b08e821cd2a98c49bac9
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-20 16:45:34 +0200

    compile khiva from source

--- .CI/travis/install-khiva.sh
+++ .CI/travis/install-khiva.sh
@@ -6,12 +6,15 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    if [ ! -e "installers/khiva-v0.1.0.pkg" ]; then
-        wget https://github.com/shapelets/khiva/releases/download/v0.1.0/khiva-v0.1.0.pkg -O installers/khiva-v0.1.0.pkg
-    fi
-
-    # Installs arrayfire
-    sudo installer -pkg installers/khiva-v0.1.0.pkg -target /
+    git clone https://github.com/shapelets/khiva.git
+    pushd khiva
+    conan remote add -f conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
+    mkdir -p build && pushd build
+    conan install .. -s compiler=apple-clang -s compiler.version=9.1 -s compiler.libcxx=libc++ --build missing
+    cmake .. -DKHIVA_ONLY_CPU_BACKEND=ON -DKHIVA_BUILD_DOCUMENTATION=OFF -DKHIVA_BUILD_EXAMPLES=OFF -DKHIVA_BUILD_BENCHMARKS=OFF -DKHIVA_BUILD_TESTS=OFF
+    make install -j8
+    popd
+    popd
 else
     if [ ! -e "installers/khiva-v0.1.0-ci.sh" ]; then
         wget https://github.com/shapelets/khiva/releases/download/v0.1.0/khiva-v0.1.0-ci.sh -O installers/khiva-v0.1.0-ci.sh
