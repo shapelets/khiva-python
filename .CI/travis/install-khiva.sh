a94646821ce53384ccbc7a375e0acde1cc7cd7ea
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-20 17:37:16 +0200

    pointing to right conan folder

--- .CI/travis/install-khiva.sh
+++ .CI/travis/install-khiva.sh
@@ -7,7 +7,7 @@
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew upgrade pyenv
-    export PATH=$HOME/.pyenv/shims:$PATH
+    export PATH=$HOME/.pyenv/shims:$HOME/.pyenv/versions/${TRAVIS_PYTHON_VERSION}/bin:$PATH
     export PYTHON_VERSION=$(echo $TRAVIS_PYTHON_VERSION | awk -F'.' '{print $1 "." $2}')
     pyenv install ${TRAVIS_PYTHON_VERSION}
     pyenv init -
