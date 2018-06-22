cb57d0666da7f884917162dc2de5eb371b8db51a
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-22 08:36:04 +0200

    Caching pyenv versions

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -9,7 +9,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew upgrade pyenv
     export PATH=$HOME/.pyenv/shims:$HOME/.pyenv/versions/${TRAVIS_PYTHON_VERSION}/bin:$PATH
     export PYTHON_VERSION=$(echo $TRAVIS_PYTHON_VERSION | awk -F'.' '{print $1 "." $2}')
-    pyenv install ${TRAVIS_PYTHON_VERSION}
+    pyenv install ${TRAVIS_PYTHON_VERSION} -s
     pyenv init -
 
     pyenv local ${TRAVIS_PYTHON_VERSION}
