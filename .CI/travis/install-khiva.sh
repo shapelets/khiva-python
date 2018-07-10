d0446c6fe785827bf936f29e850c7eff14fd287a
Antonio Vilches <antonio.vilches@shapelets.io>
2018-07-10 14:14:25 +0200

    Change travis file to allow khiva to be installed from source.

--- .CI/travis/install-khiva.sh
+++ .CI/travis/install-khiva.sh
@@ -5,8 +5,23 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-if [[$"INSTALL_KHIVA_METHOD" == "github"]]; then
-    #Installing conan and dependencies
+if [[$"INSTALL_KHIVA_METHOD" == "installers"]]; then
+   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+        if [ ! -e "./installers/khiva-v0.1.0.pkg" ]; then
+            wget https://github.com/shapelets/khiva/releases/download/v0.1.0/khiva-v0.1.0-OnlyCPU.pkg -O ./installers/khiva-v0.1.0-OnlyCPU.pkg
+        fi
+        # Installs Khiva
+        sudo installer -pkg ./installers/khiva-v0.1.0-OnlyCPU.pkg -target /
+    else
+        if [ ! -e "./installers/khiva-v0.1.0-ci.sh" ]; then
+            wget https://github.com/shapelets/khiva/releases/download/v0.1.0/khiva-v0.1.0-ci.sh -O ./installers/khiva-v0.1.0-ci.sh
+            chmod +x ./installers/khiva-v0.1.0-ci.sh
+        fi
+        sudo ./installers/khiva-v0.1.0-ci.sh --prefix=/usr/local --skip-license
+        sudo ldconfig
+    fi
+else
+     #Installing conan and dependencies
     pip install conan
     conan remote add conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
     if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
@@ -43,22 +58,6 @@ if [[$"INSTALL_KHIVA_METHOD" == "github"]]; then
         cmake .. -DKHIVA_ENABLE_COVERAGE=ON -DKHIVA_ONLY_CPU_BACKEND=ON -DKHIVA_BUILD_DOCUMENTATION=OFF -DKHIVA_BUILD_EXAMPLES=OFF -DKHIVA_BUILD_BENCHMARKS=OFF
     fi
     make install -j8
-
-else
-    if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-        if [ ! -e "./installers/khiva-v0.1.0.pkg" ]; then
-            wget https://github.com/shapelets/khiva/releases/download/v0.1.0/khiva-v0.1.0-OnlyCPU.pkg -O ./installers/khiva-v0.1.0-OnlyCPU.pkg
-        fi
-        # Installs Khiva
-        sudo installer -pkg ./installers/khiva-v0.1.0-OnlyCPU.pkg -target /
-    else
-        if [ ! -e "./installers/khiva-v0.1.0-ci.sh" ]; then
-            wget https://github.com/shapelets/khiva/releases/download/v0.1.0/khiva-v0.1.0-ci.sh -O ./installers/khiva-v0.1.0-ci.sh
-            chmod +x ./installers/khiva-v0.1.0-ci.sh
-        fi
-        sudo ./installers/khiva-v0.1.0-ci.sh --prefix=/usr/local --skip-license
-        sudo ldconfig
-    fi
 fi
 
 
