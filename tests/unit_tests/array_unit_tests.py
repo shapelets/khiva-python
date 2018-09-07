e491d35f1b114059a85307dadd013987c71b8344
Antonio Vilches <avilchesreina@gmail.com>
2018-09-07 18:34:11 +0200

    Adding a new test for Array operations (#37)
    * Adding a new test for Array operations
    
    * Deleting empty line.
    
    * Chaging the test.

--- tests/unit_tests/array_unit_tests.py
+++ tests/unit_tests/array_unit_tests.py
@@ -294,6 +294,11 @@ class ArrayTest(unittest.TestCase):
         a += b
         np.testing.assert_array_equal(a.to_numpy(), np.array([2, 4, 6, 8]))
 
+    def testIaddSelfArray(self):
+        a = Array([1, 2, 3, 4, 5])
+        a += a
+        np.testing.assert_array_equal(a.to_numpy(), np.array([2, 4, 6, 8, 10]))
+
     def testISub(self):
         a = Array([1, 2, 3, 4])
         b = Array([1, 2, 3, 4])
