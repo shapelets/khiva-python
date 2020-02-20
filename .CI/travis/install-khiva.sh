dacb0f648e007b10156c2d6560459f3249782a36
Raúl Bocanegra <raul.bocanegra@shapelets.io>
2020-02-20 11:01:03 +0100

    Fix link creation

--- .CI/travis/install-khiva.sh
+++ .CI/travis/install-khiva.sh
@@ -40,7 +40,7 @@ else
         fi
         # Install cmake
         sudo bash cmakebin/cmake-3.13.2-Linux-x86_64.sh --prefix=./cmakebin/ --skip-license
-        ln -s ./cmakebin/bin/cmake /usr/local/bin/cmake
+        sudo ln -s ./cmakebin/bin/cmake /usr/local/bin/cmake
     fi
 
     #Installing conan and dependencies
